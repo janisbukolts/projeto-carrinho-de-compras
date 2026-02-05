e1ff985429c8b2db290debac8a3e6d5501795da2
Janis Bukolts <janisbukolts@gmail.com>
2026-02-05 19:43:31 +0100

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function adicionar() {
+  //recuperar valores nome do produto, quantidade e valor
+  let nomeProduto = document.getElementById('produto');
+  let quantidade = document.getElementById('quantidade');
+
+  //calcular o preço, o nosso subtotal
+  //adicionar o produto no carrinho
+  //atualizar o valor total
+
+}
+
+function limpar() {
+
+}
